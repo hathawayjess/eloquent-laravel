851990afc1a4a77dd2e2efb006368298bb84cc4a
Jessica Hathaway <hathaway.jess@gmail.com>
2016-07-14 15:49:02 -0600

    updated CRUD notes, relationship queries

--- CRUDnotes.js
+++ CRUDnotes.js
@@ -45,7 +45,9 @@
 	//find bear with danger level greater than 5
 	$dangerousBears = Bear::where('danger_level', '>', 5)->get();
 
-	//first() will return only one record, get() will return an array of records to loop over
+
+
+	/**first() will return only one record, get() will return an array of records to loop over**/
 
 
 
@@ -79,3 +81,55 @@
 
 	//find and delete all bears with danger level over 5
 	Bear::where('danger_level', '>', 5)->delete();
+
+
+
+/*****************************************************************************************/
+
+//QUERYING RELATIONSHIPS
+
+	/***ONE-TO-ONE***/
+
+		//find a bear named Adobot
+		$adobot = Bear::where('name', '=', 'Adobot')->first();
+
+		//get the fish that Adobot has
+		$fish = $adobot->fish;
+
+		//get the weight of the fish Adobot is going to eat
+		$fish->weight;
+
+		//alternatively you could go straight to the weight attribute
+		$adobot->fish->weight;
+
+
+
+	/***ONE-TO-MANY***/
+
+		//find the trees lawly climbs
+		$lawly = Bear::where('name', '=', 'Lawly')->first();
+
+		//find the trees and their type and age
+		foreach ($lawly->trees as $tree)
+			echo $tree->type . ' ' . $tree->age;
+
+
+
+	/***MANY-TO-MANY***/
+
+
+		//get the picnics that Cerms goes to
+		$cerms = Bear::where('name', '=', 'Cerms')->first();
+
+		//get the picnics and their names/taste levels
+		foreach ($cerms->picnics as $picnic)
+			echo $picnic->name . ' ' . $picnic->taste_level;
+
+
+
+		//get the bears that go to the Grand Canyon picnic
+		$grandCanyon = Picnic::where('name', '=', 'Grand Canyon')->first();
+
+		//show the bears
+		foreach ($grandCanyon->bears as $bear)
+			echo $bear->name . ' ' . $bear->type . ' ' . $bear->danger_level;
